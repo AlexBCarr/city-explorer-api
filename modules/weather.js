aa54f54469a5f68d1d7629e2f878f042cf3f0598
Alex B Carr <mralex.carr@yahoo.com>
2023-03-23 15:47:26 -0700

    Updates

---
+++ modules/weather.js
@@ -0,0 +1,29 @@
+'use strict';
+const axios = require('axios');
+
+async function getWeather (request, response, next){
+  try {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
+
+    let weatherResults = await axios.get(url);
+    console.log(weatherResults.data);
+
+    let weatherToSendFrontEnd = weatherResults.data.data.map(dayObj => new Forecast(dayObj));
+
+    response.status(200).send(weatherToSendFrontEnd);
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+class Forecast {
+  constructor(dayObj) {
+    this.date = dayObj.date;
+    this.description = dayObj.weather.description;
+  }
+}
+
+module.exports = getWeather;
